898fafeda6478255a867607d29b55a5a623c3759
AlbertV99 <tico.urquhart@gmail.com>
2024-09-06 19:13:34 -0400

    Creacion de esquema de archivos para la pagina

---
+++ archivo/subidos/iniciarSesion.php
@@ -0,0 +1,77 @@
+<?php
+
+$consultas = null;
+try {
+    // Crear una nueva instancia de PDO para la conexión
+    $dsn = "mysql:host=locahost;dbname=asorco-archivos;charset=utf8mb4";
+    $options = [
+        PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION, // Manejar errores mediante excepciones
+        PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC, // Devolver resultados como un array asociativo
+        PDO::ATTR_EMULATE_PREPARES => false, // Desactivar la emulación de sentencias preparadas
+    ];
+    $consultas = new PDO($dsn, "root", "", []);
+
+    // Mensaje para verificar que la conexión fue exitosa
+    echo "Conexión exitosa a la base de datos.";
+} catch (PDOException $e) {
+    // Captura cualquier error de conexión y lo muestra
+    die("Error al conectar a la base de datos: " . $e->getMessage());
+}
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    if (isset($_POST["usuario"]) && isset($_POST["password"])) {
+        $consulta = new Consultas();
+        $nombre = $_POST["usuario"];
+        $pass = $_POST["password"];
+        $host = $_SERVER["HTTP_HOST"];
+        $url = $_SERVER["REQUEST_URI"];
+        $locacion_actual = "http://" . $host;
+
+        $respuesta = $consulta->query("SELECT id,usuario,cedula,pass FROM usuario ='".$nombre"'");
+
+
+        //Se verifica el pass
+        if (
+            $respuesta->rowCount() > 0
+        ) {
+            $resultado = $respuesta->fetchAll();
+            if(password_verify($pass, $respuesta[0]["pass"])){
+                session_start();
+                setcookie("PHPSESSID", session_id(), [
+                    "expires" => time() + 300, // 1 hora de duración
+                    "path" => "/",
+                    "domain" => "albertopc.com",
+                    "secure" => true, // debe ser true si usas HTTPS
+                    "httponly" => true,
+                    "samesite" => "None", // o 'Strict', según sea necesario
+                ]);
+
+                $_SESSION["usuario"] = $resultado[0]["usuario"];
+                $_SESSION["id"] = $resultado[0]["id"];
+
+                echo json_encode([
+                    "cod" => "00",
+                    "msg" => "Inicio de sesión exitoso",
+                    "user" => $resultado[0]["usuario"],
+                    "usuario" => session_id(),
+                ]); // f854a950b6344895090e835d3a8e1c58 - 4a026f189e052a854bbfc5bed359fcfa
+            }else {
+                echo json_encode([
+                    "cod" => "10",
+                    "msg" => "Credenciales incorrectas",
+                ]);
+            }
+        } else {
+            echo json_encode([
+                "cod" => "10",
+                "msg" => "Credenciales incorrectas",
+            ]);
+        }
+    } else {
+        echo json_encode([
+            "cod" => "06",
+            "msg" => "Faltan datos de inicio de sesión",
+        ]);
+    }
+} else {
+    echo json_encode(["cod" => "91", "msg" => "Método de solicitud no válido"]);
+}
---
+++ archivo/subidos/validarSesion.php
@@ -0,0 +1,43 @@
+<?php
+
+$consultas = null;
+try {
+    // Crear una nueva instancia de PDO para la conexión
+    $dsn = "mysql:host=locahost;dbname=asorco-archivos;charset=utf8mb4";
+    $options = [
+        PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION, // Manejar errores mediante excepciones
+        PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC, // Devolver resultados como un array asociativo
+        PDO::ATTR_EMULATE_PREPARES => false, // Desactivar la emulación de sentencias preparadas
+    ];
+    $consultas = new PDO($dsn, "root", "", []);
+
+    // Mensaje para verificar que la conexión fue exitosa
+    echo "Conexión exitosa a la base de datos.";
+} catch (PDOException $e) {
+    // Captura cualquier error de conexión y lo muestra
+    die("Error al conectar a la base de datos: " . $e->getMessage());
+}
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    if ($_POST["nombre_archivo"] == "" && !isset($_POST["nombre_archivo"])) {
+        // ERROR AL LEVANTAR EL ARCHIVO Redireccionamiento de pagina
+        echo "Error de archivo";
+    }
+    $usuario = $_SESSION["id"];
+    if (isset($_FILES["archivo"]["name"])) {
+        $archivo = $_FILES["archivo"]["name"];
+        $path = pathinfo($archivo);
+        $nombreArchivo = "cliente_";
+        $ext = $path["extension"];
+        $temp_name = $_FILES["foto"]["tmp_name"];
+        $nombre_nuevo = "clienteimg_" . time() . "." . $ext;
+        $path_nuevo = CARPETA_IMAGENES . "/" . $nombre_nuevo;
+        move_uploaded_file($temp_name, $path_nuevo);
+        $datos["foto"] = $path_nuevo;
+    }
+
+    $consultas->query(
+        "INSERT INTO archivos (usuario_id,nombre_archivo,nombre_mostrar,fecha_subida) VALUES ('{$usuario}','{$nombre_nuevo}','{$_POST["nombre_archivo"]}',TIMESTAMP"
+    );
+
+    // REDIRECCIONAR A LA PAGINA DE SUBIDO CORRECTAMENTE
+}
